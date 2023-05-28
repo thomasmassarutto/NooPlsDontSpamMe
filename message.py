bfda78f9330f4dda85ffdbd94d267e065dbf63b5
thomasmassarutto <thomas.massarutto@gmail.com>
2023-05-28 15:56:30 +0200

    v1.1

--- message.py
+++ message.py
@@ -7,7 +7,7 @@ def message():
     ## ad esempio solitamente bisogna togliere la perte finale
     ## ESEMPIO: 
     ## form= "https://docs.google.com/forms/d/e/randomLetters"
-    form= "https://docs.google.com/forms/d/e/1FAIpQLSea8s-v3JhTRKkY8MhW9vre6_UfW4Lag0sDkTCGQqLBaKQq2w"
+    form= ""
 
     url = form + "/formResponse"
 
@@ -17,14 +17,9 @@ def message():
     ## rispecchino quelle del form
     data = {
             #ESEMPIO:
-            ##'entry.69420666': c,
+            ##'entry.69420666': functions.generate_random_string(3,10),
             ##'entry.69420666': functions.generate_random_choiches(['Molto', 'Tanto', 'Abbastanza', 'Poco', 'Per nulla']),
             ##'entry.69420666': functions.generate_random_telephone_number(10),
-            
-            'entry.489441377': functions.generate_random_age(18,20),
-            'entry.239216472': functions.generate_random_text(7,10),
-            'entry.1597939621': functions.generate_random_choiches(['Opzione 1', 'Opzione 2']),
-            'entry.1140745251': functions.generate_random_choiches(['Opzione 1', 'Opzione 2']),
     }
 
     response = requests.post(url, data=data)
