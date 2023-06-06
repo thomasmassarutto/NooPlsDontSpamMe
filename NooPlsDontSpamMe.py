08a35a31cc781e3e2b769c910f29490ad987bb58
thomasmassarutto <thomas.massarutto@gmail.com>
2023-06-06 23:47:16 +0200

    v1.1

--- NooPlsDontSpamMe.py
+++ NooPlsDontSpamMe.py
@@ -5,7 +5,7 @@ import spam_utils
 ## Decidi quanti messaggi inviare. True se non vuoi avere un limite, consigliato inserire un valore pause
 how_much= True
 ## Decidi quanti secondi aspettare prima di inviare un nuovo messaggio. Nb
-pause= 0
+cooldown= 0
 
 i=1
 while spam_utils.i_feel_like_continuing_spam(i, how_much):
@@ -18,6 +18,6 @@ while spam_utils.i_feel_like_continuing_spam(i, how_much):
     
     i= i+1
     
-    time.sleep(pause)
+    time.sleep(cooldown)
 
 print('Fine')
